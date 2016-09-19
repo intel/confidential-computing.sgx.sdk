9cdfd100af5e668550d5e4f9d3c061db6d346774
zhaohuidu <zdu@zdu-desk.ccr.corp.intel.com>
2016-09-19 14:55:22 +0800

    Upgrade to Linux 1.6 gold release
    switch code to linux 1.6 opensource gold release

--- sdk/edger8r/linux/Preprocessor.ml
+++ sdk/edger8r/linux/Preprocessor.ml
@@ -1,4 +1,4 @@
-/*
+(*
  * Copyright (C) 2011-2016 Intel Corporation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,16 +27,45 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- */
+ *)
 
-#include <math.h>
-#include <float.h>
+open Unix
+open Printf
 
-#if (LDBL_MANT_DIG > DBL_MANT_DIG)
+open Util
 
-long double dreml(long double x, long double y)
-{
-	return remainderl(x, y);
-}
+(* for compat of OCaml before version 4.02.0 *)
+module Bytes = String
 
-#endif
+(* Run a command and return its results as a process_status*string. *)
+let read_process (command : string) : Unix.process_status * string =
+  let buffer_size = 2048 in
+  let buffer = Buffer.create buffer_size in
+  let str = Bytes.create buffer_size in
+  let in_channel = Unix.open_process_in command in
+  let chars_read = ref 1 in
+  while !chars_read <> 0 do
+    chars_read := input in_channel str 0 buffer_size;
+    Buffer.add_substring buffer str 0 !chars_read
+  done;
+  let status = Unix.close_process_in in_channel in
+  let output = Buffer.contents buffer in
+  ( status, output )
+
+(*Return None if gcc not found, caller should handle it*)
+let processor_macro ( full_path : string) : string option=
+  let gcc_path = snd (read_process "which gcc") in
+  if not (String.contains gcc_path  '/' ) then
+    (eprintf "warning: preprocessor is not found\n"; None)
+  else
+    let command = sprintf "gcc -x c -E -P \"%s\" 2>/dev/null" full_path in
+    let output = read_process command in
+    match fst output with
+      | WEXITED exit_status -> 
+        if exit_status < 0 then
+          failwithf "gcc exited with error code 0x%d\n" exit_status
+        else if exit_status > 0 then
+          failwithf "Preprocessor failed\n"
+        else
+          Some(snd output)
+      | _ -> failwithf "Preprocessor stopped by signal\n"  
