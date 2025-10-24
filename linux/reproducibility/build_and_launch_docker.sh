44d090240f2beba81fc0231c9958c437b1c713c0
Bartosz Gotowalski <bartosz.gotowalski@intel.com>
2025-10-24 18:45:27 +0200

    Update SGX SDK version to 2.27 and DCAP to 1.24
    Updated components to newest version
    
    ---------
    
    Signed-off-by: Bartosz Gotowalski <bartosz.gotowalski@intel.com>

--- linux/reproducibility/build_and_launch_docker.sh
+++ linux/reproducibility/build_and_launch_docker.sh
@@ -1,33 +1,8 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
+# Copyright(c) 2011-2025 Intel Corporation
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#   * Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-#   * Redistributions in binary form must reproduce the above copyright
-#     notice, this list of conditions and the following disclaimer in
-#     the documentation and/or other materials provided with the
-#     distribution.
-#   * Neither the name of Intel Corporation nor the names of its
-#     contributors may be used to endorse or promote products derived
-#     from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
+# SPDX-License-Identifier: BSD-3-Clause
 #
 
 # The script is to automatically prepare the reproducible code, build docker image and launch the build
@@ -75,8 +50,8 @@ mount_dir="/linux-sgx"
 sdk_installer=""
 sgx_src=""
 
-default_sdk_installer=sgx_linux_x64_sdk_reproducible_2.25.100.1.bin
-default_sdk_installer_url=https://download.01.org/intel-sgx/sgx-linux/2.25/distro/nix_reproducibility/$default_sdk_installer
+default_sdk_installer=sgx_linux_x64_sdk_reproducible_2.27.100.0.bin
+default_sdk_installer_url=https://download.01.org/intel-sgx/sgx-linux/2.27/distro/nix_reproducibility/$default_sdk_installer
 
 
 usage()
@@ -177,7 +152,7 @@ prepare_sgx_src()
     if [ "$sgx_src" != "" ]; then
         mkdir -p "$sgx_repo" && cp -a "$sgx_src/." "$sgx_repo"
     else
-        git clone -b sgx_2.25_reproducible https://github.com/intel/linux-sgx.git $sgx_repo
+        git clone -b sgx_2.27_reproducible https://github.com/intel/linux-sgx.git $sgx_repo
     fi
 
     cd "$sgx_repo" && make preparation
