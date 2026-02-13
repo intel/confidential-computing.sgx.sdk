bd2932b9177bb729f13f261fc534dbd94d0cf148
Bartosz Gotowalski <bartosz.gotowalski@intel.com>
2026-02-13 17:47:21 +0100

    Bumps prebuilds location for the 2.28 release
    Updated download_prebuild.sh script to point to 2.28 location
    
    ---------
    
    Signed-off-by: Bartosz Gotowalski <bartosz.gotowalski@intel.com>

--- download_prebuilt.sh
+++ download_prebuilt.sh
@@ -1,43 +1,16 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
+# Copyright(c) 2011-2026 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#   * Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-#   * Redistributions in binary form must reproduce the above copyright
-#     notice, this list of conditions and the following disclaimer in
-#     the documentation and/or other materials provided with the
-#     distribution.
-#   * Neither the name of Intel Corporation nor the names of its
-#     contributors may be used to endorse or promote products derived
-#     from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-#
-
 
 top_dir=`dirname $0`
 out_dir=$top_dir
-optlib_name=optimized_libs_2.26.tar.gz
-ae_file_name=prebuilt_ae_2.26.tar.gz
+optlib_name=optimized_libs_2.28.tar.gz
+ae_file_name=prebuilt_ae_2.28.tar.gz
 binutils_file_name=as.ld.objdump.r4.tar.gz
-checksum_file=SHA256SUM_prebuilt_2.26.cfg
-server_url_path=https://download.01.org/intel-sgx/sgx-linux/2.26
+checksum_file=SHA256SUM_prebuilt_2.28.cfg
+server_url_path=https://download.01.org/intel-sgx/sgx-linux/2.28
 server_optlib_url=$server_url_path/$optlib_name
 server_ae_url=$server_url_path/$ae_file_name
 server_binutils_url=$server_url_path/$binutils_file_name
