0e411ff212428ac9cd5f69e79ac484cb19d5f1f0
Bartosz Gotowalski <bartosz.gotowalski@intel.com>
2026-04-24 17:08:28 +0200

    Updates the SGX prebuilt download script to fetch the 2.29 version of the prebuilds
    Bump `optimized_libs` and `prebuilt_ae` artifact filenames from `2.28` to `2.29`
    Update the checksum config filename to `SHA256SUM_prebuilt_2.29.cfg`
    Point the download base URL to the `2.29` directory
    
    ---------
    
    Signed-off-by: Bartosz Gotowalski <bartosz.gotowalski@intel.com>

--- download_prebuilt.sh
+++ download_prebuilt.sh
@@ -6,11 +6,12 @@
 
 top_dir=`dirname $0`
 out_dir=$top_dir
-optlib_name=optimized_libs_2.28.tar.gz
-ae_file_name=prebuilt_ae_2.28.tar.gz
+VERSION=2.29
+optlib_name=optimized_libs_${VERSION}.tar.gz
+ae_file_name=prebuilt_ae_${VERSION}.tar.gz
 binutils_file_name=as.ld.objdump.r4.tar.gz
-checksum_file=SHA256SUM_prebuilt_2.28.cfg
-server_url_path=https://download.01.org/intel-sgx/sgx-linux/2.28
+checksum_file=SHA256SUM_prebuilt_${VERSION}.cfg
+server_url_path=https://download.01.org/intel-sgx/sgx-linux/${VERSION}
 server_optlib_url=$server_url_path/$optlib_name
 server_ae_url=$server_url_path/$ae_file_name
 server_binutils_url=$server_url_path/$binutils_file_name
